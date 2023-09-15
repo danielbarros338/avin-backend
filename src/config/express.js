a5078f38563d9606b0ccdcbd62366635ed40d1fa
Daniel <danielbarros338@gmail.com>
2023-09-15 13:24:35 -0300

    Implementado tratamento dos dados

--- src/config/express.js
+++ src/config/express.js
@@ -1,10 +1,13 @@
 import routes from "./../routes/routes.js";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
 
 routes(app);
 
--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,3 +1,68 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
 export async function getInfo(req, res) {
-  console.log("ok")
+  let html;
+  try {
+    const response = await axios(process.env.URL + req.params.id);
+    html = response.data;
+  } catch (err) {
+    res.json({
+      err: 400,
+      message: "Erro ao buscar os dados"
+    });
+  }
+
+  const $ = cheerio.load(html);
+  const tables = $(".w728");
+  const infos = {}
+
+  _dataProcessing($, tables, infos);
+
+  res.json({ infos });
+}
+
+function _dataProcessing($, tables, infos) {
+  tables.each(function (i) {
+    const labels = $(this).find("tbody tr .label span");
+    const values = $(this).find("tbody tr .data span");
+    const title = $(this).find("tbody tr .nivel1 span");
+    const tableData = [];
+
+    labels.each(function () {
+      const label = $(this).text().replace(/\uFFFD/gi, "");
+
+      if (label !== "?") {
+        tableData.push({ label, value: null });
+      }
+    });
+
+    values.each(function (i) {
+      const value = $(this).text().replace(/\uFFFD/gi, "");
+
+      if (value !== "?") {
+        tableData[i].value = value;
+      }
+    });
+
+    if (i === 0) {
+      infos["ResumenData"] = tableData;
+    } else if (i === 1) {
+      infos["ResumenData"] = [...infos["ResumenData"], ...tableData];
+    } else {
+      let haveTitle;
+      title.each(function () {
+        haveTitle = $(this).text().replace(/\uFFFD/gi, "").split(" ");
+        haveTitle = haveTitle.map(str => {
+          const firtChar = str.charAt(0).toUpperCase();
+
+          return firtChar + str.slice(1);
+        });
+
+        haveTitle = haveTitle.join("");
+      })
+
+      infos[haveTitle] = tableData;
+    }
+  });
 }
